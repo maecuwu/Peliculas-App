2362bd2f3f1ffbef5fb68f12a9274c0df485728c
hugogf <hugo.garciaflores@gmail.com>
2023-03-29 17:34:39 +0200

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import 'react-native-gesture-handler';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+
+export const App = () => {
+    return (
+        <NavigationContainer>
+            <Text> App </Text>
+        </NavigationContainer>
+    )
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+#Peliculas App
+
+Proyecto hecho en React Native como parte del curso de Udemy "React Native: Aplicaciones nativas en ios y Android" con el fin de aprender
